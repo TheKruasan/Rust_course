0616bbe1a483f0c9673e2bb8fe16d917c1de43a5
Russkin Artem <artemrusskin92@gmail.com>
2023-07-02 19:51:05 +0300

    done 3

--- rust_3/control_flow/src/main.rs
+++ rust_3/control_flow/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main(){
+    multiple_if();
+    array_iteration();
+    another_array_iteration();
+    loop_for();
+    endless_loop();
+}
+
+fn multiple_if() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
+
+
+fn array_iteration() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn another_array_iteration() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+fn loop_for() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn endless_loop() {
+    loop {
+        println!("again!");
+    }
 }
