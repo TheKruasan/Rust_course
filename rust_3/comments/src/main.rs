d7002af47fd3734a32aa7b8153c3f655c6ee38fb
Russkin Artem <artemrusskin92@gmail.com>
2023-07-02 19:40:08 +0300

    1

--- rust_3/comments/src/main.rs
+++ rust_3/comments/src/main.rs
@@ -1,3 +1,12 @@
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+
 fn main() {
-    println!("Hello, world!");
+    let lucky_number = 7; // I’m feeling lucky today
+}   
+
+fn mains() {
+    // I’m feeling lucky today
+    let lucky_number = 7;
 }
