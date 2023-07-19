27e8e38e81b787dee9f2012d10fa2805548bbc92
Russkin Artem <artemrusskin92@gmail.com>
2023-07-19 20:00:46 +0300

    19_1

--- rust_19/macros/src/lib.rs
+++ rust_19/macros/src/lib.rs
@@ -16,3 +16,14 @@ macro_rules! vec {
 // #[some_attribute]
 // pub fn some_name(input: TokenStream) -> TokenStream {
 // }
+
+
+#[route(GET, "/")]//example of using attribute-macros 
+fn index() {}
+
+#[proc_macro_attribute]//signature of atribue-macros
+pub fn route(attr: TokenStream, item: TokenStream) -> TokenStream {}//Here, we have two parameters of type TokenStream. ...
+                                                                    //...The first is for the contents of the attribute: the GET,...
+                                                                    //... "/" part. The second is the body of the item the...
+                                                                    //... attribute is attached to: in this case, fn index()...
+                                                                    // {} and the rest of the function’s body.
